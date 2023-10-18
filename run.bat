5fb562156662bcf5f46efe4229825dc3c9dbd378
cotruongsg <cotruongsg@gmail.com>
2023-10-18 00:59:39 -0700

    Modify run.bat

--- run.bat
+++ run.bat
@@ -1,1 +1,4 @@
+call D:\Selenium_Hydrid_Framework\venv\Scripts\activate
+timeout /t 5
 pytest -s -v -m "regression" --html=./Reports/report.html testCases/ --browser chrome
+call D:\Selenium_Hydrid_Framework\venv\Scripts\deactivate
